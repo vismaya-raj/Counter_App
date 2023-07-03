4ac0322bef22f0fc9ad8604bead21cc6fb7126ee
Vismaya <vismayavipz999@gmail.com>
2023-07-03 11:02:35 +0530

    index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import'../node_modules/bootstrap/dist/css/bootstrap.css';
 import'../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 
